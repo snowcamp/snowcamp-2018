d787e2bbeaf29728d2e1a6b8c7bb1df729c70652
ctranxuan <ctranxuan@gmail.com>
2017-06-08 14:14:02 +0200

    fix(i18n): translate the countdown

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -3,14 +3,24 @@ $(document).ready(function () {
   // Configure the countdown
 
   var countdown = $('#snc-countdown');
+  var lang = $('body').attr('lang');
   var dataTime = countdown.data('date'); // Date Format : Y/m/d
   countdown.countdown(dataTime, function (event) {
-    $(this).html(event.strftime(''
-      + '<span class="snc-days">%D <i> Jours </i></span> '
-      + '<span class="snc-hr">%H <i> Heures </i></span> '
-      + '<span class="snc-min">%M <i> Minutes </i></span> '
-      + '<span class="snc-sec">%S <i> Secondes </i></span>'
-    ));
+    if (lang == 'en') {
+      $(this).html(event.strftime(''
+        + '<span class="snc-days">%D <i> Days </i></span> '
+        + '<span class="snc-hr">%H <i> Hours </i></span> '
+        + '<span class="snc-min">%M <i> Minutes </i></span> '
+        + '<span class="snc-sec">%S <i> Seconds </i></span>'
+      ));
+    } else {
+      $(this).html(event.strftime(''
+        + '<span class="snc-days">%D <i> Jours </i></span> '
+        + '<span class="snc-hr">%H <i> Heures </i></span> '
+        + '<span class="snc-min">%M <i> Minutes </i></span> '
+        + '<span class="snc-sec">%S <i> Secondes </i></span>'
+      ));
+    }
   });
 
   // Scrolling
